e27532ef77b0c72bfa403708abb437ba952b40ac
loveplay1983 <michaelxuan@hotmail.com>
2018-06-27 13:21:13 +0800

    django demo and django basic study

---
+++ django_demo/django_test/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/opt/home_ext/maizi_python/Python_full_stack/django/django_demo/django_test/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
